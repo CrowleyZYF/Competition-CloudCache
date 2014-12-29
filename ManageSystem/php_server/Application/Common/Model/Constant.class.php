d63774cfbaca459f92ec8960abcb8c3c9f1b6fde
dy <dy2009306@mgail.com>
2014-12-29 20:25:06 +0800

    dir

--- ManageSystem/php_server/Application/Common/Model/Constant.class.php
+++ ManageSystem/php_server/Application/Common/Model/Constant.class.php
@@ -32,6 +32,6 @@ class Constant
             'used_memory' => 'used',
             'maxmemory' => 'all'
         ],
-        'scripts_dir' => '/opt/lampp/CloudCache/ManageSystem/scripts'
+        'scripts_dir' => '/opt/CloudCache/ManageSystem/scripts'
     ];
 }
