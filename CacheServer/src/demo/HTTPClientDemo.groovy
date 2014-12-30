3e836bef80224bf5a0b9c830124eae25d8b21400
RunYe <runyefly@126.com>
2014-12-31 00:16:53 +0800

    a

--- CacheServer/src/demo/HTTPClientDemo.groovy
+++ CacheServer/src/demo/HTTPClientDemo.groovy
@@ -6,29 +6,22 @@ def port = 8080
 
 def token = args.length > 1 ? args[0] : '192.168.1.113:6382';
 
-def n = args.length > 2 ? args[1] : 100
-
 HTTPCacheClient cacheClient = new HTTPCacheClient(host, port, token)
 
-long start = System.currentTimeMillis()
-for(i = 0; i < n; i++) {
-    cacheClient.set('hello', 'world')
-    println cacheClient.get('hello') == 'world'
+cacheClient.set('hello', 'world')
+println cacheClient.get('hello') == 'world'
 
-    def hash = ['a': 'b', 'c': 'd']
-    cacheClient.hashSetAll('hel', hash)
-    println cacheClient.hashGetAll('hel') == hash
+def hash = ['a': 'b', 'c': 'd']
+cacheClient.hashSetAll('hel', hash)
+println cacheClient.hashGetAll('hel') == hash
 
-    cacheClient.hashSet('hel', 'a', 'e')
-    println cacheClient.hashGet('hel', 'a') == 'e'
+cacheClient.hashSet('hel', 'a', 'e')
+println cacheClient.hashGet('hel', 'a') == 'e'
 
-    cacheClient.hashRemove('hel', 'a')
-    println cacheClient.hashGet('hel', 'a') == null
-    println cacheClient.hashSize('hel') == 1
+cacheClient.hashRemove('hel', 'a')
+println cacheClient.hashGet('hel', 'a') == null
+println cacheClient.hashSize('hel') == 1
 
-    cacheClient.get('hello', 1) //with expire
-    //Thread.currentThread().sleep(1500)
-    //println cacheClient.get('hello') == null
-}
-long end = System.currentTimeMillis()
-println end - start
+cacheClient.get('hello', 1) //with expire
+Thread.currentThread().sleep(1500)
+println cacheClient.get('hello') == null
--- CacheServer/src/demo/RMIClientDemo.groovy
+++ CacheServer/src/demo/RMIClientDemo.groovy
@@ -2,37 +2,31 @@ import java.rmi.Naming
 
 def host = '192.168.1.73'
 
-def port = 8080
+def port = 8088
 
 def token = args.length > 1 ? args[0] : '192.168.1.113:6382';
 
-def n = args.length > 2 ? args[1] : 1
-
-def cacheClientFactory = Naming.lookup("rmi://$host:8088/CacheClientFactory")
+def cacheClientFactory = Naming.lookup("rmi://$host:$port/CacheClientFactory")
 def cacheClient = cacheClientFactory.getCacheClient(token)
 
-for(i = 0; i < n; i++) {
+cacheClient.set('hello', 'world')
+println cacheClient.get('hello') == 'world'
 
-    cacheClient.set('hello', 'world')
-    println cacheClient.get('hello') == 'world'
+def hash = ['a': 'b', 'c': 'd']
+cacheClient.hashSetAll('hel', hash)
+println cacheClient.hashGetAll('hel') == hash
 
-    def hash = ['a': 'b', 'c': 'd']
-    cacheClient.hashSetAll('hel', hash)
-    println cacheClient.hashGetAll('hel') == hash
+cacheClient.hashSet('hel', 'a', 'e')
+println cacheClient.hashGet('hel', 'a') == 'e'
 
-    cacheClient.hashSet('hel', 'a', 'e')
-    println cacheClient.hashGet('hel', 'a') == 'e'
+cacheClient.hashRemove('hel', 'a')
+println cacheClient.hashGet('hel', 'a') == null
+println cacheClient.hashSize('hel') == 1
 
-    cacheClient.hashRemove('hel', 'a')
-    println cacheClient.hashGet('hel', 'a') == null
-    println cacheClient.hashSize('hel') == 1
+cacheClient.get('hello', 1) //with expire
+Thread.currentThread().sleep(1500)
+println cacheClient.get('hello') == null
 
-    cacheClient.get('hello', 1) //with expire
-    //Thread.currentThread().sleep(1500)
-    //println cacheClient.get('hello') == null
-
-    def person = new Person(name: 'hello', age: 18)
-    cacheClient.setObject('hello', person)
-    println cacheClient.getObject('hello') == person
-
-}
+def person = new Person(name: 'hello', age: 18)
+cacheClient.setObject('hello', person)
+println cacheClient.getObject('hello') == person
