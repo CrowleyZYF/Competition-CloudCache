49b3c4712d9bf6bbab86cea3893e02326819bef7
RunYe <runyefly@126.com>
2014-12-30 20:45:09 +0800

    添加最终文档

--- CacheServer/src/demo/CacheClientDemo.groovy
+++ CacheServer/src/demo/CacheClientDemo.groovy
@@ -2,20 +2,25 @@ import emem.cacheclient.CacheClient
 
 CacheClient cacheClient = new CacheClient('localhost', 8080, 'a')
 
-cacheClient.set('hello', 'world')
-println cacheClient.get('hello') == 'world'
+long start = System.currentTimeMillis()
+for(i = 0; i < 1000000; i++) {
+    cacheClient.set('hello', 'world')
+    println cacheClient.get('hello') == 'world'
 
-def hash = ['a': 'b', 'c': 'd']
-cacheClient.hashSetAll('hel', hash)
-println cacheClient.hashGetAll('hel') == hash
+    def hash = ['a': 'b', 'c': 'd']
+    cacheClient.hashSetAll('hel', hash)
+    println cacheClient.hashGetAll('hel') == hash
 
-cacheClient.hashSet('hel', 'a', 'e')
-println cacheClient.hashGet('hel', 'a') == 'e'
+    cacheClient.hashSet('hel', 'a', 'e')
+    println cacheClient.hashGet('hel', 'a') == 'e'
 
-cacheClient.hashRemove('hel', 'a')
-println cacheClient.hashGet('hel', 'a') == null
-println cacheClient.hashSize('hel') == 1
+    cacheClient.hashRemove('hel', 'a')
+    println cacheClient.hashGet('hel', 'a') == null
+    println cacheClient.hashSize('hel') == 1
 
-cacheClient.get('hello', 1) //with expire
-Thread.currentThread().sleep(1500)
-println cacheClient.get('hello') == null
+    cacheClient.get('hello', 1) //with expire
+    Thread.currentThread().sleep(1500)
+    println cacheClient.get('hello') == null
+}
+long end = System.currentTimeMillis()
+println end
